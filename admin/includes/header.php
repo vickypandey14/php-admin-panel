4d91724fd69523388b1bd840166ba6e6f17d81b5
vickypandey14 <vickypandeyts@gmail.com>
2024-05-08 10:00:38 +0530

    complete code uploaded

---
+++ admin/includes/header.php
@@ -0,0 +1,14 @@
+<div class="brand clearfix">
+<h4 class="pull-left text-white" style="margin:20px 0px 0px 20px"><i class="fa fa-rocket"></i>&nbsp; ByteWebster</h4>
+		<span class="menu-btn"><i class="fa fa-bars"></i></span>
+		<ul class="ts-profile-nav">
+			
+			<li class="ts-account">
+				<a href="#"><img src="img/ts-avatar.jpg" class="ts-avatar hidden-side" alt=""><i class="bi bi-person"></i> Your Account <i class="fa fa-angle-down hidden-side"></i></a>
+				<ul>
+					<li><a href="change-password.php"><i class="bi bi-gear-wide-connected"></i> Change Password</a></li>
+					<li><a href="logout.php"><i class="bi bi-box-arrow-left"></i> Logout</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
